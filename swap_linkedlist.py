a7c335e0d02c53dd275f3ec4b160d89b2242c08f
shuaibullattil <shuaibullattil7768@gmail.com>
2025-08-05 17:29:30 +0530

    swap in linked list

---
+++ swap_linkedlist.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+
+
+        """
+        if head == None or head.next == None:
+            return head
+    
+        dummy = ListNode(0)
+        prev = dummy
+
+    
+        while head and head.next:
+            first = head
+            second = head.next
+
+            prev.next = second
+            first.next = second.next
+            second.next = first
+
+            prev = first
+            head = first.next
+        return dummy.next
+
+        
